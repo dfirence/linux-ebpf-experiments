0e4ecfd35fc8db2afd4a90ad69ec5922bb296f2f
dfirence <carlos.diaz.pro@gmail.com>
2021-03-14 13:19:52 -0400

    add structure

---
+++ rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-4246efbac4d8f970.d
@@ -0,0 +1,5 @@
+/home/archir/Documents/my_code/rust/linux-ebpf-experiments/rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-4246efbac4d8f970.rmeta: src/main.rs
+
+/home/archir/Documents/my_code/rust/linux-ebpf-experiments/rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-4246efbac4d8f970.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-e7dfa93ed146827c.d
@@ -0,0 +1,5 @@
+/home/archir/Documents/my_code/rust/linux-ebpf-experiments/rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-e7dfa93ed146827c.rmeta: src/main.rs
+
+/home/archir/Documents/my_code/rust/linux-ebpf-experiments/rust-tooling/learn-mode/ebpf-integration/target/debug/deps/ebpf_integration-e7dfa93ed146827c.d: src/main.rs
+
+src/main.rs:
